525d4745546ae6371defd3955d88ba99931350de
carlos reina <carlosreina1234@gmail.com>
2024-08-20 21:41:39 -0500

    class19

---
+++ proyecto/clientes/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from . models import Pais, Cliente
+
+def pais_list(request):
+    paises = Pais.objects.all()
+    contexto = {'paises': paises}
+    return render(request, 'clientes/pais_list.html', contexto)
