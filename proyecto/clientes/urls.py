1bdc77d1e955790a0adad72e5dd0b7d7dc1fbc7f
carlos reina <carlosreina1234@gmail.com>
2024-08-21 18:23:59 -0500

    final

--- proyecto/clientes/urls.py
+++ proyecto/clientes/urls.py
@@ -2,6 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = "clientes"
+
 urlpatterns = [
-    path("pais/list", views.pais_list)
+    path("", views.index, name="index"),
+    path("pais/list", views.pais_list, name="pais_list"),
+    path("cliente/list", views.cliente_list, name="cliente_list")
 ]
